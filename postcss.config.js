c70a867d108450730b5d78bed8c378439e782d4c
Ben R. <33951277+tabs-not-spaces@users.noreply.github.com>
2025-02-25 12:00:25 +0000

    adding lightbox and postcss import funcrtionality to theme
    Co-authored-by: ben <ben@patchmypc.com>

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ const themeDir = __dirname;
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss')(themeDir + '/tailwind.config.js'),
     require('autoprefixer')({
       path: [themeDir]
